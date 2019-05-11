5efaf86f71711236e79b7754939e85636a5e10ab
zxb13246577 <zxb13246577@163.com>
2019-05-11 23:46:21 +0800

    init myproject

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template> 
+	<div>
+		<h1>我是App父组件----{{mydata}}</h1>
+		<son @show="show"></son>
+	</div>
+</template>
+<script>
+import son from "./son.vue"
+export default {
+	data(){
+		return{
+			mydata:""
+		}
+	},
+	methods:{
+		show(data){
+			this.mydata=data
+		}
+	},
+	components:{
+		son
+	}
+}
+</script>
+<style lang="scss" scoped>
+  
+</style>
---
+++ src/son.vue
@@ -0,0 +1,20 @@
+<template>  
+    <div>
+        <h1>我是son子组件</h1>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            msg:"我是子组件的数据"
+        }
+    },
+    mounted(){
+        this.$emit("show",this.msg)
+    }
+}
+</script>
+<style lang="scss" scoped>
+    
+</style>
